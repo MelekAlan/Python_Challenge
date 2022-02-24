b5533e194f7b8c69747c5ce4543c547b9875eeb6
melekalan <dt.bmaslan@gmail.com>
2022-02-24 20:56:19 +0300

    python file added

---
+++ Delete_nth_Number.py
@@ -0,0 +1,13 @@
+"""
+Write a Python program to remove and print every third number from a list of numbers until the list becomes empty.
+
+"""
+
+liste = [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9]
+indeks = 0
+uzunluk = len(liste)
+while len(liste) > 0 :
+    indeks = (2 + indeks) % uzunluk
+    liste.pop(indeks)
+    print(liste)
+    uzunluk -= 1
