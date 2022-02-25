a3bdb18cb070e0b55a50d86204b74c05b30914ec
melekalan <dt.bmaslan@gmail.com>
2022-02-25 22:27:30 +0300

    Find Uniq file added

---
+++ Find_Uniq.py
@@ -0,0 +1,14 @@
+"""
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
+
+find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
+
+"""
+
+
+def find_uniq(liste) :
+    for i in liste :
+        if liste.count(i) == 1 :
+            return i
