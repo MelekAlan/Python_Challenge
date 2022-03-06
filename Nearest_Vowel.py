e0fe4166b1af7d6a005cfb17a7e8396c6314e1a2
melekalan <dt.bmaslan@gmail.com>
2022-03-06 19:36:52 +0300

    Nearest Vowel file added

---
+++ Nearest_Vowel.py
@@ -0,0 +1,19 @@
+"""
+Given a letter, created a function which returns the nearest vowel to the letter. 
+If two vowels are equidistant to the given letter, return the earlier vowel.
+
+Examples
+nearestVowel("b") ➞ "a"
+nearestVowel("s") ➞ "u"
+nearestVowel("c") ➞ "a"
+nearestVowel("i") ➞ "i"
+
+Notes
+All letters will be given in lowercase.
+There will be no alphabet wrapping involved, meaning the closest vowel to "z" should return "u", not "a".
+"""
+
+def nearestVowel(string) :
+    vowel = "aeiou"
+    order = [abs(ord(i) - ord(string)) for i in vowel]
+    return vowel[order.index(min(order))]
